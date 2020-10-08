c3ff448137c093677e03762376b1deb18495e63e
hyunki <dhchung317@gmail.com>
2020-10-08 16:04:16 -0400

    setup packages and basic classes

--- app/src/main/java/com/example/datacubedrecorder/ui/MainActivity.kt
+++ app/src/main/java/com/example/datacubedrecorder/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.datacubedrecorder
+package com.example.datacubedrecorder.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.datacubedrecorder.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/datacubedrecorder/ui/fragment/SavedRecordingsFragment.kt
@@ -0,0 +1,4 @@
+package com.example.datacubedrecorder.ui.fragment
+
+class SavedRecordingsFragment {
+}
