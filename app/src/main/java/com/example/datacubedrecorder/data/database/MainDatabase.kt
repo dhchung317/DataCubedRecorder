a97ac874669a204b9e30499dde2ed6998902ef3c
hyunki <dhchung317@gmail.com>
2020-10-09 04:28:32 -0400

    database and repo connected

--- app/src/main/java/com/example/datacubedrecorder/data/database/MainDatabase.kt
+++ app/src/main/java/com/example/datacubedrecorder/data/database/MainDatabase.kt
@@ -1,9 +1,13 @@
 package com.example.datacubedrecorder.data.database
 
+import android.app.Application
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.datacubedrecorder.data.database.dao.RecordingDao
 import com.example.datacubedrecorder.data.database.model.RecordingModel
+
 
 @Database(version = 1, entities = [RecordingModel::class], exportSchema = false)
 abstract class MainDatabase : RoomDatabase() {
@@ -11,6 +15,21 @@ abstract class MainDatabase : RoomDatabase() {
     abstract fun recordingDao(): RecordingDao
 
     companion object {
-        const val DATABASE_NAME = "data.db"
+        private const val DATABASE_NAME = "data.db"
+
+        @Volatile
+        private var databaseInstance: MainDatabase? = null
+
+        fun getDatabaseInstance(context: Context): MainDatabase =
+            databaseInstance ?: synchronized(this) {
+                databaseInstance ?: buildDatabaseInstance(context).also {
+                    databaseInstance = it
+                }
+            }
+
+        private fun buildDatabaseInstance(context: Context) =
+            Room.databaseBuilder(context, MainDatabase::class.java, DATABASE_NAME)
+                .fallbackToDestructiveMigration()
+                .build()
     }
 }
--- app/src/main/java/com/example/datacubedrecorder/ui/MainViewModel.kt
+++ app/src/main/java/com/example/datacubedrecorder/ui/MainViewModel.kt
@@ -1,8 +1,25 @@
 package com.example.datacubedrecorder.ui
 
+import android.app.Application
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.datacubedrecorder.data.MainRepository
+import com.example.datacubedrecorder.data.MainRepositoryImpl
+import com.example.datacubedrecorder.data.database.MainDatabase
+import com.example.datacubedrecorder.data.database.model.RecordingModel
 
-class MainViewModel: ViewModel() {
-    //TODO methods to save data through repo
-    //TODO methods to get data through repo
+class MainViewModel(application: Application): ViewModel() {
+    private val repository: MainRepository
+
+    private val recordings: LiveData<List<RecordingModel>>
+
+    init{
+        val recordingDao = MainDatabase.getDatabaseInstance(application).recordingDao()
+        repository = MainRepositoryImpl(recordingDao)
+        recordings = repository.getAllRecordings()
+    }
+
+    fun insertRecording(recordingModel: RecordingModel){
+        repository.insertRecording(recordingModel)
+    }
 }
