692103658847c8a50f2f89a453bbc1fe99e9dd35
hyunki <dhchung317@gmail.com>
2020-10-14 02:09:26 -0400

    todo

--- app/src/main/java/com/example/datacubedrecorder/common/extensions/IntExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.example.datacubedrecorder.common.extensions
-
-import kotlin.math.floor
-
-fun Int.formatDuration(): String {
-    val minutes = floor(this.toDouble() / 60).toInt()
-    val seconds = this - minutes * 60
-    return if (seconds >= 10) "${minutes}:${seconds}" else "${minutes}:0${seconds}"
-}
